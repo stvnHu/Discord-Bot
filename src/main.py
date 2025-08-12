2cd0b8d678d801b5b1a072fb9dcb5ab06e7610d1
stvnHu <stvnhng1@gmail.com>
2025-08-12 01:29:08 +0100

    added voice_state intent

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ class Bot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
+        intents.voice_states = True
         super().__init__(command_prefix="$", intents=intents)
 
     async def setup_hook(self):
